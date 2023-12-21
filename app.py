2e61e92cd50eacfd5696d979e28fc5ce22c74e3f
jakebarnesdev <141236412+jakebarnesdev@users.noreply.github.com>
2023-12-21 16:00:15 +0000

    Update app.py for secret key name == main method

--- app.py
+++ app.py
@@ -10,13 +10,6 @@ from helpers import apology, usd
 app = Flask(__name__)
 db = SQL("sqlite:///database.db")
 
-# Check if the app is running in a production environment
-if os.environ.get("FLASK_ENV") == "production":
-    app.secret_key = os.environ.get("SECRET_KEY")
-else:
-    # If running in a development environment, use a random secret key
-    app.secret_key = os.urandom(24)
-
 app.jinja_env.filters["usd"] = usd
 
 Session(app)
@@ -414,4 +407,9 @@ def register():
 if __name__ == '__main__':
     # Use the PORT environment variable if available, otherwise default to 5000
     port = int(os.environ.get('PORT', 5000))
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    SECRET_KEY = "super secret key"
+    app.debug = True
+    app.run()
     app.run(host='0.0.0.0', port=port, debug=False)
